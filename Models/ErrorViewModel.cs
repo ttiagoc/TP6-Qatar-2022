b8c43fd9d2b0eb2ff73d0a13815e2fe24420269d
Android Studio <devandroid@ort>
2022-07-06 11:32:27 -0300

    Create MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP6_Qatar_2022.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
